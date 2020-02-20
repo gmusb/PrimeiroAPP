6e6b29337a665d1c79f8f6a72358e54be5bc56eb
gmusb <musbmg@gmail.com>
2020-02-21 02:16:13 +0900

    Adicionando o layout e funcionalidades

---
+++ www/components/contar.js
@@ -0,0 +1,27 @@
+$(document).on("click","#adicao",function(){
+  var v1 = $("#valor1").val();
+  var v2 = $("#valor2").val();
+  var result = parseFloat(v1)+parseFloat(v2);
+  $("#resultado").val(result);
+});
+$(document).on("click","#subitracao",function(){
+  var v1 = $("#valor1").val();
+  var v2 = $("#valor2").val();
+  var result = parseFloat(v1)-parseFloat(v2);
+  $("#resultado").val(result);
+});
+$(document).on("click","#multiplicacao",function(){
+  var v1 = $("#valor1").val();
+  var v2 = $("#valor2").val();
+  var result = parseFloat(v1)*parseFloat(v2);
+  $("#resultado").val(result);
+});
+$(document).on("click","#divisao",function(){
+  var v1 = $("#valor1").val();
+  var v2 = $("#valor2").val();
+  var result = parseFloat(v1)/parseFloat(v2);
+  $("#resultado").val(result);
+});
+$(document).on("click","#limpar",function(){
+  $("#resultado").val("");
+});
